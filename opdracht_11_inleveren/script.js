6585096cd38c4b313bac61fd40833a04bf444062
MarritNL <86615967+MarritNL@users.noreply.github.com>
2021-07-21 16:51:49 +0200

    assigment 11

---
+++ opdracht_11_inleveren/script.js
@@ -0,0 +1,21 @@
+console.log('hello winc academy');
+
+let name = 'Marrit'
+console.log(name);
+
+
+let number = 4
+console.log(number*number);
+
+
+
+let x=10
+let y=5*x
+console.log (y);
+
+age = 38
+console.log (typeof 38);
+
+age = 'thirtyeight'
+console.log (typeof 'thirtyeight');
+
